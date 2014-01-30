9be40945395ae3a58144478f97630cdc3ff7f3a9
Jesús Javier Nuño García <jesus.nuno@neofonie.de>
2014-01-30 11:36:02 +0100

    Fixed problem with the https connection (DO NOT PUT THIS IN PRODUCTION).

--- src/main/java/com/android/jjnunogarcia/SimpleTripPlanner/requests/RemoteConnectionParser.java
+++ src/main/java/com/android/jjnunogarcia/SimpleTripPlanner/requests/RemoteConnectionParser.java
@@ -1,19 +1,20 @@
 package com.android.jjnunogarcia.SimpleTripPlanner.requests;
 
+import android.util.Log;
 import com.android.jjnunogarcia.SimpleTripPlanner.model.Location;
 import com.android.jjnunogarcia.SimpleTripPlanner.requests.RemoteLocationParsingAsyncTask.RemoteLocationParsingInterface;
 import com.google.android.gms.maps.model.LatLng;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import javax.net.ssl.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 
 /**
@@ -22,6 +23,7 @@ import java.util.ArrayList;
  * Parses the json response from an url and creates objects according to the data received.
  */
 public class RemoteConnectionParser {
+  public static final String TAG                = RemoteConnectionParser.class.getSimpleName();
   public static final String JSON_RESULTS       = "results";
   public static final String JSON_TYPE          = "_type";
   public static final String JSON_ID            = "_id";
@@ -50,37 +52,56 @@ public class RemoteConnectionParser {
   }
 
   private JSONObject readJsonFromUrl() throws IOException, JSONException {
-    DefaultHttpClient httpClient = new DefaultHttpClient();
-    HttpGet getRequest = new HttpGet(url);
-    getRequest.addHeader("accept", "application/json");
+    HttpURLConnection http;
+    URL theUrl = new URL(url);
 
-    HttpResponse response = httpClient.execute(getRequest);
-
-    if (response.getStatusLine().getStatusCode() != 200) {
-      throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
+    if (theUrl.getProtocol().toLowerCase().equals("https")) {
+      trustAllHosts();
+      HttpsURLConnection https = (HttpsURLConnection) theUrl.openConnection();
+      https.setHostnameVerifier(DO_NOT_VERIFY);
+      http = https;
+    } else {
+      http = (HttpURLConnection) theUrl.openConnection();
     }
 
-    BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+    String jsonText = getJSON(http, 30000);
 
-    String jsonText = readAll(br);
-//    String output;
-//    System.out.println("Output from Server .... \n");
-//    while ((output = br.readLine()) != null) {
-//      System.out.println(output);
-//    }
-
-    httpClient.getConnectionManager().shutdown();
     return new JSONObject(jsonText);
+  }
 
-//    HttpGet httpGet = new HttpGet(url);
-//    InputStream is = new URL(url).openStream();
-//    try {
-//      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName(HTTP.UTF_8)));
-//      String jsonText = readAll(rd);
-//      return new JSONObject(jsonText);
-//    } finally {
-//      is.close();
-//    }
+  private String getJSON(HttpURLConnection http, int timeout) {
+    InputStream inputStream = null;
+    try {
+      http.setRequestMethod("GET");
+      http.setRequestProperty("Content-length", "0");
+      http.setUseCaches(false);
+      http.setAllowUserInteraction(false);
+      http.setConnectTimeout(timeout);
+      http.setReadTimeout(timeout);
+      http.connect();
+      int status = http.getResponseCode();
+
+      switch (status) {
+        case 200:
+        case 201:
+          inputStream = http.getInputStream();
+          BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+          return readAll(br);
+      }
+    } catch (MalformedURLException ex) {
+      Log.e(TAG, "Error retrieving Json", ex);
+    } catch (IOException ex) {
+      Log.e(TAG, "Error retrieving Json", ex);
+    } finally {
+      if (inputStream != null) {
+        try {
+          inputStream.close();
+        } catch (IOException e) {
+          Log.e(TAG, "Error closing stream", e);
+        }
+      }
+    }
+    return null;
   }
 
   private ArrayList<Location> buildLocationsFromJsonObject(JSONObject jsonObject) throws JSONException {
@@ -141,7 +162,45 @@ public class RemoteConnectionParser {
     return sb.toString();
   }
 
+  /*
+   * Always verify the host - don't check for certificate
+   */
+  private final static HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+    public boolean verify(String hostname, SSLSession session) {
+      return true;
+    }
+  };
+
+  /**
+   * This doesn't  check any certificate. That means, it trusts every server.
+   * DO NOT USE THIS IN PRODUCTION
+   */
+  private static void trustAllHosts() {
+    // Create a trust manager that does not validate certificate chains
+    TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+      public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+        return new java.security.cert.X509Certificate[] {};
+      }
+
+      public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+      }
+
+      public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+      }
+    }};
+
+    // Install the all-trusting trust manager
+    try {
+      SSLContext sc = SSLContext.getInstance("TLS");
+      sc.init(null, trustAllCerts, new java.security.SecureRandom());
+      HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    } catch (Exception e) {
+      Log.e(TAG, "Error installing the all-trusting certificate", e);
+    }
+  }
+
   public void setRemoteParsingInterface(RemoteLocationParsingInterface remoteParsingInterface) {
     this.remoteParsingInterface = remoteParsingInterface;
   }
+
 }
